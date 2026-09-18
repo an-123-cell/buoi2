48c36c649d4b267eaf0ff06b27c60e45fcefe4ae
thanhnhan-B23DCDT184 <thanhnhan1502912@gmail.com>
2026-09-18 08:09:07 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,187 +1,192 @@
 #include <stdint.h>
 
-#define RCC_APB2ENR  (*(volatile uint32_t*)0x40021018)
+#define RCC_APB2ENR (*(volatile uint32_t*)0x40021018)
+#define RCC_APB1ENR (*(volatile uint32_t*)0x4002101C)
 
-#define GPIOA_CRL    (*(volatile uint32_t*)0x40010800)
-#define GPIOA_CRH    (*(volatile uint32_t*)0x40010804)
+#define GPIOA_CRL (*(volatile uint32_t*)0x40010800)
+#define GPIOA_CRH (*(volatile uint32_t*)0x40010804)
 
-#define USART1_SR    (*(volatile uint32_t*)0x40013800)
-#define USART1_DR    (*(volatile uint32_t*)0x40013804)
-#define USART1_BRR   (*(volatile uint32_t*)0x40013808)
-#define USART1_CR1   (*(volatile uint32_t*)0x4001380C)
+#define USART1_SR (*(volatile uint32_t*)0x40013800)
+#define USART1_DR (*(volatile uint32_t*)0x40013804)
+#define USART1_BRR (*(volatile uint32_t*)0x40013808)
+#define USART1_CR1 (*(volatile uint32_t*)0x4001380C)
 
-#define ADC1_SR      (*(volatile uint32_t*)0x40012400)
-#define ADC1_CR2     (*(volatile uint32_t*)0x40012408)
-#define ADC1_SMPR2   (*(volatile uint32_t*)0x40012410)
-#define ADC1_SQR1    (*(volatile uint32_t*)0x4001242C)
-#define ADC1_SQR3    (*(volatile uint32_t*)0x40012434)
-#define ADC1_DR      (*(volatile uint32_t*)0x4001244C)
+#define TIM2_CR1 (*(volatile uint32_t*)0x40000000)
+#define TIM2_EGR (*(volatile uint32_t*)0x40000014)
+#define TIM2_CCMR1 (*(volatile uint32_t*)0x40000018)
+#define TIM2_CCER (*(volatile uint32_t*)0x40000020)
+#define TIM2_PSC (*(volatile uint32_t*)0x40000028)
+#define TIM2_ARR (*(volatile uint32_t*)0x4000002C)
+#define TIM2_CCR1 (*(volatile uint32_t*)0x40000034)
 
-#define SYST_CSR     (*(volatile uint32_t*)0xE000E010)
-#define SYST_RVR     (*(volatile uint32_t*)0xE000E014)
-#define SYST_CVR     (*(volatile uint32_t*)0xE000E018)
-
-#define MA_LOP  "Lop01"
-#define MA_NHOM "- N09"
+void USART1_IRQHandler(void)
+{
+}
 
 void UART1_Init(void)
 {
-    RCC_APB2ENR |= (1 << 2);
-    RCC_APB2ENR |= (1 << 14);
+    RCC_APB2ENR |= (1<<2);
+    RCC_APB2ENR |= (1<<14);
 
-    /* PA9 TX */
-    GPIOA_CRH &= ~(0xF << 4);
-    GPIOA_CRH |= (0xB << 4);
+    GPIOA_CRH &= ~(0xF<<4);
+    GPIOA_CRH |= (0xB<<4);
 
-    /* PA10 RX */
-    GPIOA_CRH &= ~(0xF << 8);
-    GPIOA_CRH |= (0x4 << 8);
+    GPIOA_CRH &= ~(0xF<<8);
+    GPIOA_CRH |= (0x4<<8);
 
-    /* 115200 baud - clock 8MHz */
     USART1_BRR = 0x45;
 
-    USART1_CR1 = (1 << 13) | (1 << 3) | (1 << 2);
+    USART1_CR1 = 0;
+    USART1_CR1 |= (1<<13);
+    USART1_CR1 |= (1<<3);
+    USART1_CR1 |= (1<<2);
 }
 
-void UART1_SendChar(char c)
+void UART_SendChar(char c)
 {
-    while (!(USART1_SR & (1 << 7)));
+    while(!(USART1_SR & (1<<7)));
     USART1_DR = c;
 }
 
-void UART1_SendString(char *str)
+char UART_ReceiveChar(void)
 {
-    while (*str)
-    {
-        UART1_SendChar(*str);
-        str++;
-    }
+    while(!(USART1_SR & (1<<5)));
+    return (char)USART1_DR;
 }
 
-void UART1_SendNumber(uint32_t num)
+void UART_SendString(char *s)
 {
-    char buffer[10];
-    uint32_t i = 0;
-
-    if (num == 0)
-    {
-        UART1_SendChar('0');
-        return;
-    }
-
-    while (num > 0)
-    {
-        buffer[i] = (num % 10) + '0';
-        num = num / 10;
-        i++;
-    }
-
-    while (i > 0)
-    {
-        i--;
-        UART1_SendChar(buffer[i]);
-    }
+    while(*s)
+        UART_SendChar(*s++);
 }
 
-void ADC1_Init(void)
+void TIM2_Init(void)
 {
-    /* Enable ADC1 clock */
-    RCC_APB2ENR |= (1 << 9);
+    RCC_APB1ENR |= (1<<0);
 
-    /* PA0 = Analog input */
-    GPIOA_CRL &= ~(0xF << 0);
+    GPIOA_CRL &= ~(0xF<<0);
+    GPIOA_CRL |= (0xB<<0);
 
-    /* Channel 0 sample time = 239.5 cycles */
-    ADC1_SMPR2 &= ~(0x7 << 0);
-    ADC1_SMPR2 |= (0x7 << 0);
+    TIM2_PSC = 7;
+    TIM2_ARR = 999;
 
-    /* One conversion */
-    ADC1_SQR1 = 0;
+    TIM2_CCMR1 = 0;
+    TIM2_CCMR1 |= (6<<4);
+    TIM2_CCMR1 |= (1<<3);
 
-    /* First conversion = Channel 0 */
-    ADC1_SQR3 = 0;
+    TIM2_CCER |= (1<<0);
 
-    /* ADC ON */
-    ADC1_CR2 = (1 << 0);
+    TIM2_CCR1 = 0;
 
-    /* Wait */
-    for (volatile uint32_t i = 0; i < 10000; i++);
-
-    /* Reset calibration */
-    ADC1_CR2 |= (1 << 3);
-
-    while (ADC1_CR2 & (1 << 3));
-
-    /* Start calibration */
-    ADC1_CR2 |= (1 << 2);
-
-    while (ADC1_CR2 & (1 << 2));
-
-    /* Software trigger */
-    ADC1_CR2 |= (1 << 20);
-    ADC1_CR2 |= (7 << 17);
+    TIM2_EGR |= (1<<0);
+    TIM2_CR1 |= (1<<0);
 }
 
-uint16_t ADC1_Read(void)
+void PWM_Set(uint8_t percent)
 {
-    ADC1_SR &= ~(1 << 1);
+    if(percent > 100)
+        percent = 100;
 
-    ADC1_CR2 |= (1 << 22);
-
-    while (!(ADC1_SR & (1 << 1)));
-
-    return (uint16_t)(ADC1_DR & 0xFFF);
+    TIM2_CCR1 = ((TIM2_ARR + 1) * percent) / 100;
 }
 
-void SysTick(void)
+void LED_ON(void)
 {
-    /* 8MHz / 8000 = 1kHz = 1ms */
-    SYST_RVR = 8000 - 1;
-    SYST_CVR = 0;
+    PWM_Set(50);
+}
 
-    /* Enable SysTick, processor clock */
-    SYST_CSR = 5;
-
-    /* 1000 x 1ms = 1 second */
-    for (volatile uint32_t i = 0; i < 1000; i++)
-    {while (!(SYST_CSR & (1 << 16)));
-    }
-
-    SYST_CSR = 0;
+void LED_OFF(void)
+{
+    TIM2_CCR1 = 0;
 }
 
 int main(void)
 {
-    uint16_t adc_value;
-    uint32_t voltage;
+    char buffer[50];
+    uint8_t index=0;
+    char c;
 
     UART1_Init();
-    ADC1_Init();
+    TIM2_Init();
 
-    UART1_SendString("\r\n");
-    UART1_SendString("================================\r\n");
-    UART1_SendString("BAI 3 - CAM BIEN ANH SANG\r\n");
-    UART1_SendString(MA_LOP);
-    UART1_SendString(MA_NHOM);
-    UART1_SendString("\r\n");
-    UART1_SendString("================================\r\n");
+    LED_OFF();
 
-    while (1)
+    UART_SendString("\r\nBAI 5 UART PWM READY\r\n");
+    UART_SendString("Nhap lenh va ket thuc bang !\r\n");
+    UART_SendString("ON!  OFF!  PWM:50!  Status!\r\n");
+    UART_SendString("> ");
+
+    while(1)
     {
-        adc_value = ADC1_Read();
+        c = UART_ReceiveChar();
 
-        voltage = ((uint32_t)adc_value * 3300) / 4095;
+        if(c == '!')
+        {
+            buffer[index] = '\0';
 
-        UART1_SendString("ADC = ");
-        UART1_SendNumber(adc_value);
+            UART_SendString("\r\nBan vua nhap: ");
+            UART_SendString(buffer);
+            UART_SendString("\r\n");
 
-        UART1_SendString(" | Voltage = ");
-        UART1_SendNumber(voltage);
+            if(buffer[0]=='O' && buffer[1]=='N' && buffer[2]=='\0')
+            {
+                LED_ON();
+                UART_SendString("LED ON - PWM 50%\r\n");
+            }
+            else if(buffer[0]=='O' && buffer[1]=='F' && buffer[2]=='F' && buffer[3]=='\0')
+            {
+                LED_OFF();
+                UART_SendString("LED OFF - PWM 0%\r\n");
+            }
+            else if(buffer[0]=='P' && buffer[1]=='W' && buffer[2]=='M' && buffer[3]==':' )
+            {uint8_t value=0;
+                uint8_t i=4;
 
-        UART1_SendString(" mV\r\n");
+                while(buffer[i]>='0' && buffer[i]<='9')
+                {
+                    value = value*10 + (buffer[i]-'0');
+                    i++;
+                }
 
-        SysTick();
+                if(value<=100)
+                {
+                    PWM_Set(value);
+                    UART_SendString("PWM = ");
+
+                    if(value>=100)
+                        UART_SendChar('1');
+
+                    if(value>=10)
+                        UART_SendChar('0'+((value/10)%10));
+
+                    UART_SendChar('0'+(value%10));
+                    UART_SendString("%\r\n");
+                }
+                else
+                {
+                    UART_SendString("Invalid PWM\r\n");
+                }
+            }
+            else if(buffer[0]=='S' && buffer[1]=='t' && buffer[2]=='a' && buffer[3]=='t' && buffer[4]=='u' && buffer[5]=='s' && buffer[6]=='\0')
+            {
+                UART_SendString("Status OK\r\n");
+            }
+            else
+            {
+                UART_SendString("Invalid command\r\n");
+            }
+
+            index=0;
+            UART_SendString("> ");
+        }
+        else
+        {
+            if(index<49)
+            {
+                buffer[index] = c;
+                index++;
+                UART_SendChar(c);
+            }
+        }
     }
-
-    return 0;
 }
--- startup.s
+++ startup.s
@@ -5,9 +5,12 @@
 .global _estack
 .global Reset_Handler
 .global SysTick_Handler
+.global USART1_IRQHandler
+
 .extern main
 
 .section .isr_vector,"a",%progbits
+
 .word _estack
 .word Reset_Handler
 .word Default_Handler
@@ -25,9 +28,31 @@
 .word Default_Handler
 .word SysTick_Handler
 
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word Default_Handler
+.word USART1_IRQHandler
+
 .section .text.Reset_Handler
 .thumb_func
-.type Reset_Handler, %function
 
 Reset_Handler:
     bl main
@@ -37,14 +62,12 @@ loop:
 
 .section .text.Default_Handler
 .thumb_func
-.type Default_Handler, %function
 
 Default_Handler:
     b Default_Handler
 
 .section .text.SysTick_Handler
 .thumb_func
-.type SysTick_Handler, %function
 
 SysTick_Handler:
     bx lr
